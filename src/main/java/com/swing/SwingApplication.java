7d10d97032c9b38059a0aa964d531010bea6bc1f
troy <398798254@qq.com>
2019-05-08 20:34:40 +0800

    initial project

---
+++ src/main/java/com/swing/SwingApplication.java
@@ -0,0 +1,17 @@
+package com.swing;
+
+/**
+ * author Troy
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwingApplication.class, args);
+    }
+
+}
